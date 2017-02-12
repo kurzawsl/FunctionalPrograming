9c2f33c298f9dffda9ac0b5950d9f4185577b930
Lukasz Kurzawski <lkurzawski@gmail.com>
2017-02-12 01:39:32 +0100

    Adding solution to FirstClassCitizenVariable

--- src/com/rxjava/FirstClassCitizenVariable.java
+++ src/com/rxjava/FirstClassCitizenVariable.java
@@ -1,8 +1,21 @@
 package com.rxjava;
 
+import java.util.function.BiFunction;
+
 public class FirstClassCitizenVariable {
 	
 	public static void main(String[] args) {
+		BiFunction<String, String, String> concatFunction = (s,t)->  s+t;
+		System.out.println(concatFunction.apply("hello ", "world1"));
+		
+		concatFunction = FirstClassCitizenVariable::concatStrings;
+		System.out.println(concatFunction.apply("hello ", "world2"));
+		
+		FirstClassCitizenVariable instance = new FirstClassCitizenVariable();
+		
+		concatFunction = instance::concatStrings2;
+		
+		System.out.println(concatFunction.apply("hello ", "world3"));
 		
 	}
 	
@@ -10,7 +23,7 @@ public class FirstClassCitizenVariable {
 		return a + b;
 	}
 	
-	String concatString2(String a, String b){
+	String concatStrings2(String a, String b){
 		return a + b;
 	}
 
