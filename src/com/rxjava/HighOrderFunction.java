6520a64e7d88648314477cb5879aa8f03e7c13d2
Lukasz Kurzawski <lkurzawski@gmail.com>
2017-02-12 02:06:57 +0100

    Adding answer to HighOrderFunction

--- src/com/rxjava/HighOrderFunction.java
+++ src/com/rxjava/HighOrderFunction.java
@@ -1,9 +1,31 @@
 package com.rxjava;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class HighOrderFunction {
 
 	public static void main(String[] args) {
+		Supplier<String> xformOperation = createCombineAndTransform("Hello ", "world", a->a.toUpperCase());
 		
+		System.out.println(xformOperation.get());
 	}
 
+	public static Supplier<String> createCombineAndTransform(
+			final String a, final String b,
+			final Function<String, String> transformer
+			){
+				return () -> {
+					String aa = a;
+					String bb = b;
+					
+					if(transformer != null){
+						aa = transformer.apply(a);
+						bb = transformer.apply(b);
+					}
+					
+					return aa + bb;
+				};
+		
+	}
 }
