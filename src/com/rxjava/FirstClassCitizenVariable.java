5db795b373688a8a15148fb6039b6eeaa93e438c
Lukasz Kurzawski <lkurzawski@gmail.com>
2017-02-12 01:29:40 +0100

    Initial commit

---
+++ src/com/rxjava/FirstClassCitizenVariable.java
@@ -0,0 +1,17 @@
+package com.rxjava;
+
+public class FirstClassCitizenVariable {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private static String concatStrings(String a, String b){
+		return a + b;
+	}
+	
+	String concatString2(String a, String b){
+		return a + b;
+	}
+
+}
